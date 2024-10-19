57886a559d5514de11eb725eb687f9a8ee533cbd
Miguel Leite <162582246+Miguel48569@users.noreply.github.com>
2024-10-18 22:03:27 -0300

    Add files via upload

---
+++ SQL Scripts(deletar ou atualizar DML).sql
@@ -0,0 +1,137 @@
+update músicos
+set nome = "Marcos de Palha"
+where idMúsicos = 1;
+
+UPDATE Sinfonias 
+SET descricao = 'Sinfonia revisada e ampliada para a nova temporada.' 
+WHERE idSinfonias = 3;
+
+UPDATE FuncaoDosMusicos 
+SET nomeFuncao = 'Coordenador de Ensaios' 
+WHERE idFuncaoDosMusicos = 4;
+
+UPDATE FuncaoDosMusicos 
+SET descricaoFuncao = 'Responsável por ensaios e suporte adicional'
+WHERE nomeFuncao = 'Maestro';
+
+UPDATE Musicos 
+SET nome = 'Gabriel Almeida'
+WHERE idMusico = 3;
+
+UPDATE Orquestras 
+SET nomeOrquestra = 'Orquestra Filarmônica Nacional' 
+WHERE idOrquestras = 1;
+
+UPDATE FuncaoDosMusicos 
+SET descricaoFuncao = 'Líder do grupo de percussão.' 
+WHERE idFuncaoDosMusicos = 6;
+
+UPDATE FuncaoDosMusicos 
+SET dataFim = '2023-12-31'
+where idFuncaoDosMusicos = 5;
+
+UPDATE FuncaoDosMusicos 
+SET nomeFuncao = 'Líder da seção de metais' 
+WHERE idFuncaoDosMusicos = 5;
+
+update funcaodosmusicos
+SET descricaoFuncao = 'Acompanha outros músicos com uma guitarra'
+where idFuncaoDosMusicos = 5;
+
+UPDATE FuncaoDosMusicos 
+SET dataInicio = DATE_ADD(dataInicio, INTERVAL 2 MONTH) 
+WHERE idFuncaoDosMusicos = 4;
+
+-- desativei: modo safe updates (atualizações seguras) está ativado
+SET SQL_SAFE_UPDATES = 0;
+
+UPDATE FuncaoDosMusicos 
+SET dataFim = '2023-12-31';
+
+-- ativei de volta
+SET SQL_SAFE_UPDATES = 1;
+
+UPDATE músicos 
+SET nome = 'Carlos Silva'
+WHERE idMusico = 8;
+
+UPDATE FuncaoDosMusicos 
+SET descricaoFuncao = 'Líder do grupo de percussão.' 
+WHERE idFuncaoDosMusicos = 6;
+
+UPDATE Sinfonias 
+SET nomeSinfonia = 'Sinfonia Primavera' 
+WHERE idSinfonias = 3;
+
+UPDATE Músicos 
+SET nome = 'João Gabriel de Medeiros Neto' 
+WHERE idMusico = 4;
+
+-- desativei: modo safe updates (atualizações seguras) está ativado
+SET SQL_SAFE_UPDATES = 0;
+
+DELETE FROM FuncaoDosMusicos 
+WHERE nomeFuncao = 'Contrabaixista';
+
+DELETE FROM FuncaoDosMusicos 
+WHERE Musicos_idMusicos = 7;
+
+DELETE FROM Orquestras 
+WHERE nomeOrquestra = 'Orquestra Sinfônica Local';
+
+DELETE FROM Orquestras 
+WHERE idOrquestras NOT IN (SELECT Orquestras_idOrquestras FROM Sinfonias);
+
+DELETE FROM FuncaoDosMusicos 
+WHERE idFuncaoDosMusicos = 5;
+
+DELETE FROM FuncaoDosMusicos 
+WHERE Musicos_idMusicos = 7;
+
+SET SQL_SAFE_UPDATES = 1;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
